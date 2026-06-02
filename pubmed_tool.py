6ef06b8567db99aef85fcc729169401714326a1b
Khaled Ragab <khaledmohamedragab1@gmail.com>
2026-06-02 22:27:17 +0300

    Fetch PubMed article details

--- pubmed_tool.py
+++ pubmed_tool.py
@@ -1,7 +1,9 @@
 import requests
+import xml.etree.ElementTree as ET
 
 
-BASE_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+SEARCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+FETCH_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
 
 
 def search_pubmed(query, max_results=5):
@@ -12,7 +14,7 @@ def search_pubmed(query, max_results=5):
         "retmax": max_results,
     }
 
-    response = requests.get(BASE_URL, params=params)
+    response = requests.get(SEARCH_URL, params=params)
     response.raise_for_status()
 
     data = response.json()
@@ -21,9 +23,48 @@ def search_pubmed(query, max_results=5):
     return pmids
 
 
+def fetch_article_details(pmids):
+    params = {
+        "db": "pubmed",
+        "id": ",".join(pmids),
+        "retmode": "xml",
+    }
+
+    response = requests.get(FETCH_URL, params=params)
+    response.raise_for_status()
+
+    root = ET.fromstring(response.text)
+
+    articles = []
+
+    for article in root.findall(".//PubmedArticle"):
+        pmid = article.findtext(".//PMID")
+        title = article.findtext(".//ArticleTitle")
+        abstract_parts = article.findall(".//AbstractText")
+
+        abstract = " ".join(
+            part.text for part in abstract_parts if part.text
+        )
+
+        articles.append({
+            "pmid": pmid,
+            "title": title,
+            "abstract": abstract,
+        })
+
+    return articles
+
+
 if __name__ == "__main__":
     query = "implementation Ethiopia"
-    pmids = search_pubmed(query, max_results=5)
+    pmids = search_pubmed(query, max_results=3)
+    articles = fetch_article_details(pmids)
 
     print("Query:", query)
     print("PMIDs:", pmids)
+
+    for article in articles:
+        print("-" * 40)
+        print("PMID:", article["pmid"])
+        print("Title:", article["title"])
+        print("Abstract:", article["abstract"][:500])
